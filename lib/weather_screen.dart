420724449c5f15edfa491420300d2a26c4335e66
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-07-31 22:15:34 +0700

    finish main card

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -21,25 +23,71 @@ class WeatherScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Column(
-        children: [
-          // main card
-          Placeholder(
-            fallbackHeight: 250,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Placeholder(
-            fallbackHeight: 150,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Placeholder(
-            fallbackHeight: 150,
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 10,
+                      sigmaY: 10,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            "300°K",
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Icon(
+                            Icons.cloud,
+                            size: 64,
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            "Rain",
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Placeholder(
+              fallbackHeight: 150,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Placeholder(
+              fallbackHeight: 150,
+            ),
+          ],
+        ),
       ),
     );
   }
