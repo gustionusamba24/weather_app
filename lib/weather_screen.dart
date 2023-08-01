62490affb2ab3463c046a794fea24a8ce438b681
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-08-01 10:43:46 +0700

    create card for weather forecast

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -80,7 +80,41 @@ class WeatherScreen extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "03.00",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          "320.12",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
             const Placeholder(
               fallbackHeight: 150,
             ),
